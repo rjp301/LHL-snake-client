2e2c3557758f59481795060ded4491f157a1d864
rjp301 <rileypaul96@gmail.com>
2022-01-19 14:45:03 -0700

    add user input handling

--- play.js
+++ play.js
@@ -1,8 +1,15 @@
 const net = require('net');
-const connect = require('./client')
+const connect = require('./client');
+const setupInput = require('./setupInput');
 
 console.log('Connecting...');
 const conn = connect();
+const stdin = setupInput();
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === '\u0003') { process.exit() };
+};
 
 conn.on("connect", () => {
   console.log("Connected to server");
@@ -12,3 +19,5 @@ conn.on("connect", () => {
 conn.on("data", (data) => {
   console.log("Incoming from server: ",data);
 })
+
+stdin.on("data", handleUserInput)
---
+++ setupInput.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
